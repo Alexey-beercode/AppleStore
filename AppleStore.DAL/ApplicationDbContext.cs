8971a5b00ab1e6bc57d4ecacec19528dca76ee3c
Alexey-beercode <alsemkovbn@gmail.com>
2023-05-14 16:24:26 +0300

    Upload DAL

---
+++ AppleStore.DAL/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AppleStore.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppleStore.DAL;
+
+public class ApplicationDbContext: DbContext
+{
+    public DbSet<Device?> Device { get; set; } = null!;
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
---
+++ AppleStore.DAL/Interfaces/IDeviceRepository.cs
@@ -0,0 +1,9 @@
+﻿using AppleStore.Domain.Entity;
+
+namespace AppleStore.DAL.Interfaces;
+
+public interface IDeviceRepository : IBaseRepository<Device>
+{
+    Task<Device> GetByName(string? name);
+    
+}
