6d5b924f6f763643e273809ac4d99ed05b915591
Alexey-beercode <alsemkovbn@gmail.com>
2023-08-08 00:12:35 +0300

    Added error debaging

--- AppleStore.Domain/Enum/DeviceType.cs
+++ AppleStore.Domain/Enum/DeviceType.cs
@@ -4,8 +4,8 @@ namespace AppleStore.Domain.DeviceType;
 
 public enum DeviceType
 {
-    [Display(Name = "Часы")] Watch,
-    [Display(Name = "Телефон")] Phone,
-    [Display(Name = "Планшет")] Tablet,
-    [Display(Name = "Ноутбук")] Laptop
+    [Display(Name = "Часы")] Watch=1,
+    [Display(Name = "Телефон")] Phone=0,
+    [Display(Name = "Планшет")] Tablet=2,
+    [Display(Name = "Ноутбук")] Laptop=3
 }
